567bacb9ad58a25849bd49e0ecd0c6ada7527a59
Abhishek Yadav <107461726+abhishekyadav001@users.noreply.github.com>
2025-10-18 11:03:43 +0530

    change cors so that i dont get the errors

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,9 @@ const taskRoutes = require('./routes/task.routes');
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173',process.env.FRONTEND_URL], // Add your frontend URLs
-  credentials: true
+  // origin: ['http://localhost:3000', 'http://localhost:5173',process.env.FRONTEND_URL], // Add your frontend URLs
+  // credentials: true
+  *
 }));
 app.use(express.json());
 app.use(morgan('dev'));
